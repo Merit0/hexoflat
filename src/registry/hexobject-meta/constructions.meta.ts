33a2d53ec4b1acc2e956e491c5edb5e96547dd71
oleh.semeniuk <oleh.semeniuk@avenga.com>
2026-02-27 15:45:13 +0100

    fix hero navigation

--- src/registry/hexobject-meta/constructions.meta.ts
+++ src/registry/hexobject-meta/constructions.meta.ts
@@ -1,5 +1,7 @@
 import { HEXOBJECT_KEYS } from "@/registry/hexobjects-registry";
 import {IHexobjectMeta} from "@/registry/hexobject-meta/hexobject-meta-abstraction";
+import {EHexActionType} from "@/enums/hex-action-type";
+import {HeroToolType} from "@/enums/hero-tool-type";
 
 type TConstructionMetaKeys =
     | typeof HEXOBJECT_KEYS.CAMPING_ENTRANCE
@@ -13,39 +15,63 @@ export const CONSTRUCTION_META: Record<TConstructionMetaKeys, IHexobjectMeta> = 
         key: HEXOBJECT_KEYS.CAMPING_ENTRANCE,
         title: "Camp",
         subtitle: "Entrance",
-        actions: {},
-        yields: {},
+        actions: {
+            [EHexActionType.ENTER]: {
+                label: "Enter",
+                durationMs: 400,
+                requiredTool: HeroToolType.HAND,
+            },
+        },
+        enter: {
+            type: "WORLD",
+            locationKey: "camping",
+        },
     },
 
     [HEXOBJECT_KEYS.CAVE_ENTRANCE]: {
         key: HEXOBJECT_KEYS.CAVE_ENTRANCE,
         title: "Cave",
         subtitle: "Entrance",
-        actions: {},
-        yields: {},
+        actions: {
+            [EHexActionType.ENTER]: {
+                label: "Enter",
+                durationMs: 400,
+                requiredTool: HeroToolType.HAND,
+            },
+        },
+        enter: {
+            type: "WORLD",
+            locationKey: "cave",
+        },
     },
 
     [HEXOBJECT_KEYS.HOMELAND_GATE]: {
         key: HEXOBJECT_KEYS.HOMELAND_GATE,
         title: "Homeland Gate",
         subtitle: "Entrance",
-        actions: {},
-        yields: {},
+        actions: {
+            [EHexActionType.ENTER]: {
+                label: "Enter",
+                durationMs: 400,
+                requiredTool: HeroToolType.HAND,
+            },
+        },
+        enter: {
+            type: "WORLD",
+            locationKey: "silesia",
+        },
     },
 
     [HEXOBJECT_KEYS.FIREPLACE]: {
         key: HEXOBJECT_KEYS.FIREPLACE,
         title: "Fireplace",
         subtitle: "Rest spot",
-        actions: {},
-        yields: {},
+        actions: {}
     },
 
     [HEXOBJECT_KEYS.WOOD_AND_LEAVES]: {
         key: HEXOBJECT_KEYS.WOOD_AND_LEAVES,
         title: "Nature",
-        subtitle: "Decoration",
-        actions: {},
-        yields: {},
+        subtitle: "Decoration"
     },
 };
